0ede16dd36d634dc3dce165225555181f97fac47
Raff-dev <56380303+Raff-dev@users.noreply.github.com>
2020-12-16 22:54:03 +0100

    create Movie model

--- CodeAddict/Movies/models.py
+++ CodeAddict/Movies/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from Profiles.models import Profile
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=30, unique=True)
+
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    description = models.CharField(max_length=300, null=True)
+
+    rating = models.IntegerField(null=True, validators=[
+        MaxValueValidator(10),
+        MinValueValidator(1)
+    ])
+
+    class Meta():
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
