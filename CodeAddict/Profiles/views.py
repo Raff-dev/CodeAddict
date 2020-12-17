5d5b6e041a9f52cd6f3268d38a62cb390bb53856
Raff-dev <56380303+Raff-dev@users.noreply.github.com>
2020-12-17 01:24:40 +0100

    add register APIView

--- CodeAddict/Profiles/views.py
+++ CodeAddict/Profiles/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ViewSet
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import permission_classes
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import status, generics
+from django.core.serializers import serialize
+from django.http import JsonResponse
+from Profiles.models import Profile
+from Movies.models import Movie, Ticket
+from Movies.serializers import MovieSerializer
+from .Serializers import RegisterSerializer, UserSerializer
 
-# Create your views here.
+from django.contrib.auth.models import User
+
+
+# Register API
+class Register(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args,  **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        print(request.data)
+
+        serializer.is_valid(raise_exception=True)
+        print('here')
+
+        user = serializer.save()
+        print('here')
+        result = {
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "message": "User Created Successfully.  Now perform Login to get your token",
+        }
+        return Response(data=result, status=HTTP_201_CREATED)
